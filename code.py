692871245eb6c1baa6e54c7f4ff34b38b54304fa
Michael Crimando <crimandomichael@yahoo.com>
2024-02-23 04:07:18 -0500

    Works!
    Note Commands all come form the DVI keyboard. why? hmm   
    
     8: Adafruit Feather RP2040 DVI Consumer Control
          Classes: KEYBOARD | DPAD | JOYSTICK | EXTERNAL
          Path: /dev/input/event4
          Enabled: true
          Descriptor: b23b085742452a08992a86f4043cfdf01b032fc3
          Location: usb-xhci-hcd.1.auto-1.3/input3
          ControllerNumber: 0
          UniqueId: DF625857C72A202C
          Identifier: bus=0x0003, vendor=0x239a, product=0x8128, version=0x0111
          KeyLayoutFile: /system/usr/keylayout/Generic.kl
          KeyCharacterMapFile: /system/usr/keychars/Generic.kcm
          ConfigurationFile:
          VideoDevice: <none>
        9: Adafruit Feather RP2040 DVI
          Classes: KEYBOARD | GAMEPAD | JOYSTICK | EXTERNAL
          Path: /dev/input/event5
          Enabled: true
          Descriptor: 78dae7c9b707b149b34581aa6e7023a136480128
          Location: usb-xhci-hcd.1.auto-1.3/input3
          ControllerNumber: 1
          UniqueId: DF625857C72A202C
          Identifier: bus=0x0003, vendor=0x239a, product=0x8128, version=0x0111
          KeyLayoutFile: /system/usr/keylayout/Generic.kl
          KeyCharacterMapFile: /system/usr/keychars/Generic.kcm
          ConfigurationFile:
          VideoDevice: <none>
        7: Adafruit Feather RP2040 DVI Mouse
          Classes: CURSOR | EXTERNAL
          Path: /dev/input/event3
          Enabled: true
          Descriptor: 269c0c081bf465a3c83676508cd7f713176a6ab9
          Location: usb-xhci-hcd.1.auto-1.3/input3
          ControllerNumber: 0
          UniqueId: DF625857C72A202C
          Identifier: bus=0x0003, vendor=0x239a, product=0x8128, version=0x0111
          KeyLayoutFile:
          KeyCharacterMapFile:
          ConfigurationFile:
          VideoDevice: <none>
        6: Adafruit Feather RP2040 DVI Keyboard
          Classes: KEYBOARD | ALPHAKEY | EXTERNAL
          Path: /dev/input/event2
          Enabled: true
          Descriptor: 682ad8a82dfb296cc3a25bf6c3293be3c072aa0c
          Location: usb-xhci-hcd.1.auto-1.3/input3
          ControllerNumber: 0
          UniqueId: DF625857C72A202C
          Identifier: bus=0x0003, vendor=0x239a, product=0x8128, version=0x0111
          KeyLayoutFile: /system/usr/keylayout/Generic.kl
          KeyCharacterMapFile: /system/usr/keychars/Generic.kcm
          ConfigurationFile:
          VideoDevice: <none>

--- code.py
+++ code.py
@@ -1,12 +1,37 @@
 import board
 import digitalio
 import time
+import usb_hid
+from hid_gamepad import Gamepad
+
+gp = Gamepad(usb_hid.devices)
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+# Equivalent of Arduino's map() function.
+def range_map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
 while True:
+    gp.press_buttons(1)
+    print(" press", 1, end="")
+    gp.move_joysticks(
+    x=range_map(500, 0, 65535, -127, 127),
+    y=range_map(500, 0, 65535, -127, 127),
+    )
+    print(" x", 500, "y", 500)
     led.value = True
-    time.sleep(0.5)
+    time.sleep(1)
+
+
+
+    gp.release_buttons(1)
+    gp.move_joysticks(
+    x=range_map(10000, 0, 65535, -127, 127),
+    y=range_map(10000, 0, 65535, -127, 127),
+    )
+    print(" x", 10000, "y", 10000)
+    print(" release", 1, end="")
     led.value = False
-    time.sleep(0.5)
+    time.sleep(1)
